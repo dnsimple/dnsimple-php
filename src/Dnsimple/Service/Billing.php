427b0198cf03c0e88066d30fbc2e1b112370d169
Stephen Nelson <47795400+AGS4NO@users.noreply.github.com>
2023-10-25 12:34:25 -0400

    Update Billing.php

--- src/Dnsimple/Service/Billing.php
+++ src/Dnsimple/Service/Billing.php
@@ -6,38 +6,6 @@ use Dnsimple\DnsimpleException;
 use Dnsimple\Response;
 use Dnsimple\Struct\Charge;
 
-
-// '/{account}/billing/charges':
-//     get:
-//       description: Lists the billing charges for the account.
-//       parameters:
-//         - $ref: '#/components/parameters/Account'
-//         - $ref: '#/components/parameters/FilterStartDate'
-//         - $ref: '#/components/parameters/FilterEndDate'
-//         - $ref: '#/components/parameters/SortCharges'
-//       operationId: listCharges
-//       tags:
-//         - billing
-//       summary: List billing charges
-//       responses:
-//         '200':
-//           content:
-//             application/json:
-//               schema:
-//                 properties:
-//                   data:
-//                     type: array
-//                     items:
-//                       $ref: '#/components/schemas/Charge'
-//                   pagination:
-//                     $ref: '#/components/schemas/Pagination'
-//                 type: object
-//           description: Billing charges listing.
-//         '401':
-//           $ref: '#/components/responses/401'
-//         '429':
-//           $ref: '#/components/responses/429'
-
 /**
  * Lists the billing charges the authenticated entity has access to.
  *
